af3171658c9c75d0322015e05a4a5f35d15fc1b5
Luke <94769656+lukem570@users.noreply.github.com>
2025-10-19 19:21:44 -0400

    Init

---
+++ include/linalg/linalg.hpp
@@ -0,0 +1,17 @@
+#ifndef LINALG_HPP
+#define LINALG_HPP
+
+namespace Linalg {
+
+    template <const unsigned int ...N>
+    class Tensor {
+        
+    };
+
+    template <const unsigned int N>
+    class Vector {
+
+    };
+}
+
+#endif
