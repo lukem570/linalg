7f39c8e27ab22397a6a18dca00fba0fb7ac2375e
Luke <94769656+lukem570@users.noreply.github.com>
2025-10-23 19:08:25 -0400

    Update linalg.hpp

--- include/linalg/linalg.hpp
+++ include/linalg/linalg.hpp
@@ -135,7 +135,7 @@ namespace Linalg {
 
             TensorT() = default;
             TensorT(std::initializer_list<float> list) {
-                std::copy(list.begin(), list.end(), data.end());
+                std::copy(list.begin(), list.end(), data);
             }
 
             float& operator[](std::size_t index) {
@@ -214,7 +214,7 @@ namespace Linalg {
 
             TensorT() = default;
             TensorT(std::initializer_list<TensorT<typename PopBack<Dim>::value>> list) {
-                std::copy(list.begin(), list.end(), data.end());
+                std::copy(list.begin(), list.end(), data);
             }
 
             template <int D1, int D2>
